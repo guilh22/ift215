cf190ac7908598fc66581840d0e0050c380534d4
Guillaume Harvey <guilh_22@hotmail.com>
2013-03-21 22:12:35 -0400

    modify all header and style.css

--- view/headerConAd.php
+++ view/headerConAd.php
@@ -4,6 +4,7 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>IFT215</title>
         <link href="css/style.css" rel="stylesheet" type="text/css" />
+        <link href="css/style2.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <div class="wrapper"> <?php // end in footer.php ?>
