07a91f7a2420d9ba3fbe18a756101cdeede026a1
Guillaume Harvey <guilh_22@hotmail.com>
2013-03-21 21:06:04 -0400

    modify menu from headercon.php

---
+++ view/headerConAd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>IFT215</title>
+    </head>
+    <body>
+        <div class="wrapper"> <?php // end in footer.php ?>
+            <header>
+                <img id="logo" src="./images/header/logo.jpg" alt="No Man’Z Lan Logo"/>
+                <div id = "connexion">
+                    <h2>Bonjour, <?php // add name of user here ?></h2>
+                    Prochain événement <br />
+                    No Man’Z Lan : OP Gamer’Z Style <br />
+                    <?php // add Date function here ?>
+                    <img id="inscrire" src="./images/header/inscrire.jpg"/>
+                    <img id="Déconnexion" src="./images/header/deconnexion.jpg"/>
+                </div>
+                <nav>
+                    <ul>
+                        <li><a href="./view/forum.php">Forum</a></li>
+                        <li><a href="./view/evenement.php">Évènement</a></li>
+                        <li><a href="./view/evenement.php">Information</a>
+                            <ul>
+                                <li><a href="./view/coordonnee.php">Coordonnée</a></li>
+                                <li><a href="./view/equipe.php">Équipe</a></li>
+                                <li><a href="./view/equipement.php">Équipement</a></li>
+                                <li><a href="./view/historique.php">Coordonnée</a></li>
+                                <li><a href="./view/reglements.php">Règlements</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="./view/calendrier.php">Calendrier</a></li>
+                        <li><a href="./view/faq.php">FAQ</a></li>
+                        <li><a href="./view/votre_espace.php">Votre espace</a></li>
+                    </ul>
+                </nav>
+           </header>
