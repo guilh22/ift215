fff3d9396b4e5688fa38857c4d690b5ec2c886ba
Guillaume Harvey <guilh_22@hotmail.com>
2013-03-21 20:36:47 -0400

    add menu to headercon.php

---
+++ view/headerCon.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>IFT215</title>
+    </head>
+    <body>
+        <div class="wrapper"> <?php // end in footer.php ?>
+            <header>
+                <img id="logo" src="./images/header/logo.jpg" alt="No Man’Z Lan Logo"/>
+                <div id = "connexion">
+                    <h2>Bonjour, <?php // add name of user here ?></h2>
+                    Prochain événement <br />
+                    No Man’Z Lan : OP Gamer’Z Style <br />
+                    <?php // add Date function here ?>
+                    <img id="inscrire" src="./images/header/inscrire.jpg"/>
+                    <img id="Déconnexion" src="./images/header/deconnexion.jpg"/>
+                </div>
+           </header>
