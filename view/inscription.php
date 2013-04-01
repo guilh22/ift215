cdf9ce5a8566d2188db3834d4e93c58ad3692f96
Guillaume Harvey <guilh_22@hotmail.com>
2013-04-01 17:45:07 -0400

    add fleche and flecheExp in view/images/evenement add inscription.php and start code it modify evenement

---
+++ view/inscription.php
@@ -0,0 +1,28 @@
+<h1>Inscription</h1>
+<div>
+    <form>
+        <div class="nom">
+        Information personnelle
+        </div>
+        <label>Nom *: </label>: 
+        <input type="text" id="champNom" name="nom" size="30"/>
+        <label>Prenom *: </label>
+        <input type="text" id="champPrenom" name="prenom" size="30"/></br>
+        <label>Email *: </label>
+        <input type="text" id="champPrenom" name="prenom" size="30" placeholder="toto@toto.ca"/></br>
+        <div class="nom">
+            Information public
+        </div>
+        <label>Surnom *: </label>
+        <input type="text" id="champSurnom" name="surnom" size="30"/></br>
+        <label>Avatar: </label>
+        <input type="text" id="avatar" name="avatar" size="30" placeholder="Parcourir"/></br>
+        <div class="nom">
+            Clan
+        </div>
+        <input type="radio" id="existant" value="Existant" name="clan" checked/>
+        <label>Existant
+        <input type="radio" id="existant" value="Creation" name="clan"/>
+        <label>Création
+    </form>
+</div>
