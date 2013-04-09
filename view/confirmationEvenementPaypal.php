12c1fb6f6278390e779cec6a0b3278cc2f6e6231
Guillaume Harvey <guilh_22@hotmail.com>
2013-04-08 23:35:00 -0400

    commit
    commit

---
+++ view/confirmationEvenementPaypal.php
@@ -0,0 +1,4 @@
+<h1>Inscription</h1>
+<p>
+    Votre inscription à l'évènement est terminée, un courriel vous a été envoyé afin de confirmer votre inscription.
+</p>
--- view/inscriptionLan3.php
+++ view/inscriptionLan3.php
@@ -1,10 +1,13 @@
 <h1>Inscription au Lan</h1>
 <div class="nom step3">Étape 3</div>
-<img src="view/images/inscriptionLan/planSalle.jpg"/></br>
-<img src="view/images/inscriptionLan/planSalle.jpg"/>Libre
-<img src="view/images/inscriptionLan/planSalle.jpg"/>Réserver
-<img src="view/images/inscriptionLan/planSalle.jpg"/>Organisateur</br>
-<form>
-    Réserver la place #: <input type="text" class="place" size="15" onkeypress='return event.charCode >= 48 && event.charCode <= 57' onkeyup="if(jQuery('.place').val() != ''){jQuery('.finish').removeAttr('disabled');if(jQuery('.place').val() > '20' || jQuery('.place').val() < '0'){jQuery('.finish').attr('disabled','disabled');alert('La valeur doit être plus grande que 0 ou plus petite que 20');}}else{jQuery('.finish').attr('disabled','disabled');}"/></br>
-    <input type="submit" class="finish" value="Terminer" disabled/>
-</form>
+<div id="imageReservation">
+    <form method="post" action="?page=paypalConfirm">
+        <img id="place" src="view/images/inscriptionLan/place.png"/></br>
+        <img id="libre" src="view/images/inscriptionLan/libre.png"/><label id="lblLibre">Libre</label>
+        <img id="reserver" src="view/images/inscriptionLan/reserver.png"/><label id="lblReserver">Réserver</label>
+        <img id="organisateur" src="view/images/inscriptionLan/organisateur.png"/><label id="lblOrganisateur">Organisateur</label></br>
+
+        <label for="champReserver" id="lblChampReserver">Réserver la place # : </label><input type="text" id="champReserver" class="place" size="15" onkeypress='return event.charCode >= 48 && event.charCode <= 57' onkeyup="if(jQuery('.place').val() != ''){jQuery('.finish').removeAttr('disabled');if(jQuery('.place').val() > 60 || jQuery('.place').val() < 0){jQuery('.finish').attr('disabled','disabled');alert('La valeur doit être plus grande que 0 ou plus petite ou égal à 60');}}else{jQuery('.finish').attr('disabled','disabled');}"/></br>
+        <input type="submit" class="finish" value="Terminer" disabled/>
+    </form>
+</div>
